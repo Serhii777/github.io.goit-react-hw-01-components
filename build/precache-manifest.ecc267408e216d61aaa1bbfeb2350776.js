32b5de297c88419316a42c671fc1076b790f7ed2
Serhii777 <muzykasv72@gmail.com>
2020-09-23 01:35:54 +0300

    Added folder build

---
+++ build/precache-manifest.ecc267408e216d61aaa1bbfeb2350776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8410595063f59d04ef63744c79eb73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e51ba444c3013c8da211",
+    "url": "/static/css/main.7e5a338f.chunk.css"
+  },
+  {
+    "revision": "cfced537f0bfd968f512",
+    "url": "/static/js/2.b1dee16d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1dee16d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51ba444c3013c8da211",
+    "url": "/static/js/main.6512025a.chunk.js"
+  },
+  {
+    "revision": "21775e121504a9fc29a1",
+    "url": "/static/js/runtime-main.c1392d30.js"
+  }
+]);
